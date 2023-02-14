6b5a6fe923b76ddff6ad342540eb82db815c9107
spmackey <spmackey@ncsu.edu>
2023-02-14 13:37:33 -0500

    Created Profile Page

--- private/shared/user_header.php
+++ private/shared/user_header.php
@@ -19,9 +19,9 @@
 
         <?php 
         require_login();
-        echo "Username: ". $session->username . "<br>";
+        echo "Welcome ". $session->username . "!<br>";
         echo "User Level: ". $session->user_level ."<br>";
-
+        echo "<a href='". url_for("/profile") ."'>User Profile</a>";
         echo "<a href='". url_for("logout.php") ."'>Logout</a><br>";
         include(SHARED_PATH . "/navbar.php");
         ?>
