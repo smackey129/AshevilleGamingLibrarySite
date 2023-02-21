7c265c814e9674897834e68d37a3b0cb9cd5981a
spmackey <spmackey@ncsu.edu>
2023-02-21 13:54:03 -0500

    Created Index Pages for admin sub areas

---
+++ admin/games/index.php
@@ -0,0 +1,37 @@
+<?php 
+  require_once('../../private/initialize.php');   
+  $games = Game::find_all();
+  $page_title = 'Games'; 
+  include(SHARED_PATH . '/admin_header.php'); 
+?>
+
+<h2>Games</h2>
+
+  	<table border="1">
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>ESRB Rating</th>
+        <th>Minimum Age</th>
+        <th>Genre</th>
+        <th>Publisher</th>
+        <th>View</th>
+        <th>Edit</th>
+        <th>Delete</th>
+      </tr>
+
+      <?php foreach($games as $game) { ?>
+        <tr>
+          <td><?= h($game->id); ?></td>
+          <td><?= h($game->name_gme); ?></td>
+          <td><?= h($game->getRating()); ?></td>
+          <td><?= h($game->getMinAge()); ?></td>
+          <td><?= h($game->getGenre()); ?></td>
+          <td><?= h($game->getPublisher()); ?></td>
+          <td><a href="<?php echo url_for('admin/games/show.php?id=' . h(u($game->id))); ?>">View</a></td>
+          <td><a href="<?php echo url_for('admin/games/edit.php?id=' . h(u($game->id))); ?>">Edit</a></td>
+          <td><a href="<?php echo url_for('admin/games/delete.php?id=' . h(u($game->id))); ?>">Delete</a></td>
+    	  </tr>
+      <?php } ?>
+  	</table>
+<?php include(SHARED_PATH . '/user_footer.php'); ?>
---
+++ admin/inventory/index.php
@@ -0,0 +1,42 @@
+<?php 
+  require_once('../../private/initialize.php');   
+  $inventory = InventoryItem::find_all();
+  $page_title = 'Inventory'; 
+  include(SHARED_PATH . '/admin_header.php'); 
+?>
+
+<h2>Inventory</h2>
+
+  	<table border="1">
+      <tr>
+        <th>ID</th>
+        <th>Game</th>
+        <th>Console</th>
+        <th>Condition</th>
+        <th>Current User</th>
+        <th>Donator</th>
+        <th>Available?</th>
+        <th>Available After</th>
+        <th>View</th>
+        <th>Edit</th>
+        <th>Delete</th>
+      </tr>
+
+      <?php foreach($inventory as $item) { ?>
+        <tr>
+          <td><?= h($item->id); ?></td>
+          <td><?= h($item->getGame()); ?></td>
+          <td><?= h($item->getConsole()); ?></td>
+          <td><?= h($item->condition_inv); ?></td>
+          <td><?= h($item->getCurrentUser()); ?></td>
+          <td><?= h($item->getDonator()); ?></td>
+          <td><?= h($item->getAvailability()); ?></td>
+          <td><?= h($item->available_after_inv); ?></td>
+          <td><a href="<?php echo url_for('admin/inventory/show.php?id=' . h(u($item->id))); ?>">View</a></td>
+          <td><a href="<?php echo url_for('admin/inventory/edit.php?id=' . h(u($item->id))); ?>">Edit</a></td>
+          <td><a href="<?php echo url_for('admin/inventory/delete.php?id=' . h(u($item->id))); ?>">Delete</a></td>
+    	  </tr>
+      <?php } ?>
+  	</table>
+
+<?php include(SHARED_PATH . '/user_footer.php'); ?>
