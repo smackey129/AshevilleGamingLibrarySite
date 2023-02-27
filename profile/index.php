ea5fbc54aa3701abb6ed28de6330dce63eff8dad
spmackey <spmackey@ncsu.edu>
2023-02-26 19:06:47 -0500

    Added Links to new Pages

--- profile/index.php
+++ profile/index.php
@@ -7,7 +7,9 @@ include(SHARED_PATH . '/user_header.php');
 
 <h1><?= $session->username ."'s Profile Page"?></h1>
 
+<a href="editinfo.php">Edit My Information</a>
 <a href="mydonations.php">My Donated Games</a>
 <a href="mycheckouts.php">My Checked Out Games</a>
+<a href="wishlist.php">My Wishlist</a>
 
 <?php include(SHARED_PATH . '/user_footer.php');
