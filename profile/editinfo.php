8c2cb75974408f4b91ba24ada3a5c736d5561f1a
spmackey <spmackey@ncsu.edu>
2023-02-26 19:05:52 -0500

    Created Edit User Information Page

---
+++ profile/editinfo.php
@@ -0,0 +1,54 @@
+<?php require_once('../private/initialize.php');   ?>
+
+<?php
+
+$user = User::find_by_username($session->username);
+if($user == false) {
+  redirect_to('index.php');
+}
+
+if(is_post_request()) {
+
+  // Save record using post parameters
+  $args = $_POST['user'];
+  $user->merge_attributes($args);
+  $result = $user->save();
+  if($result === true) {
+    $session->message('Information Updated');
+    $session->login($user);
+  } else {
+    // show errors
+  }
+
+} else {
+
+  // display the form
+
+}
+
+?>
+
+<?php $page_title = 'Edit My Information'; ?>
+<?php include(SHARED_PATH . '/user_header.php'); ?>
+
+<div id="content">
+
+  <div class="bicycle edit">
+    <h2>Edit My Information</h2>
+
+    <?php echo display_errors($user->errors); ?>
+
+    <form action="editinfo.php" method="post">
+
+      <?php include('../users/form_fields.php'); ?>
+
+      <div id="operations">
+        <input type="submit" value="Save Changes" />
+      </div>
+    </form>
+
+  </div>
+
+</div>
+
+<?php include(SHARED_PATH . '/user_footer.php'); ?>
