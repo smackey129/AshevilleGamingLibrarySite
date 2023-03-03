d6d54981f0da0c68ee273239d8596f484bd0d0c6
spmackey <spmackey@ncsu.edu>
2023-03-03 13:09:55 -0500

    Updated Wish List Page

--- profile/wishlist.php
+++ profile/wishlist.php
@@ -1,29 +1,31 @@
 <?php require_once('../private/initialize.php'); ?>
 <?php 
-$page_title = $session->username . '\'s Profile'; 
+$page_title = $session->username . '\'s Wish List'; 
 include(SHARED_PATH . '/user_header.php'); 
 $user = User::find_by_username($session->username);
 $checkouts = $user->getCheckouts();
-
+$games = $user->getWishList();
 ?>
 
 
 <h2>My Wish List (Placeholder Content)</h2>
 
-  	<table border="1">
+<table border="1">
       <tr>
-        <th>Game</th>
-        <th>Console</th>
-        <th>Condition</th>
-        <th>Due By</th>
+        <th>Name</th>
+        <th>ESRB Rating</th>
+        <th>Minimum Age</th>
+        <th>Genre</th>
+        <th>Publisher</th>
       </tr>
 
-      <?php foreach($checkouts as $item) { ?>
+      <?php foreach($games as $game) { ?>
         <tr>
-          <td><?= h($item->getGame()); ?></td>
-          <td><?= h($item->getConsole()); ?></td>
-          <td><?= h($item->condition_inv); ?></td>
-          <td><?= h($item->available_after_inv); ?></td>
+          <td><?= h($game->name_gme); ?></td>
+          <td><?= h($game->getRating()); ?></td>
+          <td><?= h($game->getMinAge()); ?></td>
+          <td><?= h($game->getGenre()); ?></td>
+          <td><?= h($game->getPublisher()); ?></td>
     	  </tr>
       <?php } ?>
   	</table>
