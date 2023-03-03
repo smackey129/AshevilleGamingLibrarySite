e2c9b2a4317b8b18a683b59e72fdd80e33bcf2e6
spmackey <spmackey@ncsu.edu>
2023-03-03 13:11:37 -0500

    Added Content To Page

---
+++ catalog/checkout.php
@@ -0,0 +1,29 @@
+<?php require_once('../private/initialize.php');   ?>
+
+<?php
+
+if(!isset($_GET['id']) || !is_post_request()) {
+  redirect_to('index.php');
+}
+
+$id = $_GET['id'];
+$item = InventoryItem::find_by_id($id);
+$user = User::find_by_username($session->username);
+if(!$item->available_inv) {
+  redirect_to('index.php');
+}
+
+$game = Game::find_by_id($item->id_gme_inv);
+
+if($game == false) {
+  redirect_to('index.php');
+}
+?>
+
+<?php $page_title = 'Checkout: ' . h($game->name_gme); ?>
+<?php include(SHARED_PATH . '/user_header.php'); ?>
+<main>
+  <h2>Thank You!</h2>
+  <?php $item->checkout($user); ?>
+</main>
+<?php include(SHARED_PATH . '/user_footer.php'); ?>
