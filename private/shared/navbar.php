db03124ee2ba5ae4d8c5aa8625946957d30f0e91
spmackey <spmackey@ncsu.edu>
2023-03-09 18:06:17 -0500

    Changed Navbar Text

--- private/shared/navbar.php
+++ private/shared/navbar.php
@@ -1,5 +1,5 @@
 <ul id="navbar">
   <li><a href=<?= url_for("index.php")?>>Home</a></li>
   <li><a href=<?= url_for("about.php")?>>About Us</a></li>
-  <li><a href=<?= url_for("/catalog")?>>Game Catalog</a></li>
+  <li><a href=<?= url_for("/catalog")?>>Catalog</a></li>
 </ul>
