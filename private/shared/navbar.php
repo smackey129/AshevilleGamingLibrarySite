b8334a00a5d38949006fc429300c29544785b64f
spmackey <spmackey@ncsu.edu>
2023-03-12 19:37:26 -0400

    Added Donate Section

--- private/shared/navbar.php
+++ private/shared/navbar.php
@@ -2,4 +2,5 @@
   <li><a href=<?= url_for("index.php")?>>Home</a></li>
   <li><a href=<?= url_for("about.php")?>>About Us</a></li>
   <li><a href=<?= url_for("/catalog")?>>Catalog</a></li>
+  <li><a href=<?= url_for("/donate")?>>Donate</a></li>
 </ul>
