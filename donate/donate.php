a1d66d3649e8076f621e87ca027e4ed7449e1f9f
spmackey <spmackey@ncsu.edu>
2023-03-24 13:29:22 -0400

    Added new game form

--- donate/donate.php
+++ donate/donate.php
@@ -2,27 +2,41 @@
 require_once('../private/initialize.php');
 
 if(is_post_request()) {
-
-  // Create record using post parameters
+  $item = new InventoryItem;
   $args = $_POST['item'];
-  $item = new InventoryItem($args);
-  $user = User::find_by_username($session->username);
-  $item->id_usrdonator_inv = $user->id;
-  $item->available_inv = 0;
-  $item->available_after_inv = date("Y-m-d");
-  $result = $item->save();
-
-  if($result === true) {
-    $new_id = $item->id;
-    $session->message('The item was created successfully.');
-    redirect_to('index.php');
-  } else {
-    // show errors
+  if($args['id_gme_inv'] == -1) {
+    $game_args = $_POST['game'];
+    $new_game = new Game($game_args);
+    $game_result = $new_game->save();
+    if($game_result === true) {
+      $new_id = $new_game->id;
+      $args['id_gme_inv'] = $new_id;
+    } 
   }
+  else {
+    $game_result = true;
+  }
+  if($game_result === true) {
+    $item = new InventoryItem($args);
+    $user = User::find_by_username($session->username);
+    $item->id_usrdonator_inv = $user->id;
+    $item->available_inv = 0;
+    $item->available_after_inv = date("Y-m-d");
+    $result = $item->save();
+    if($result === true) {
+      $new_id = $item->id;
+      $session->message('Thank you for your donation!');
+      redirect_to('index.php');
+    } else {
+      // show errors
+    }
+  }
+  
 
 } else {
   // display the form
   $item = new InventoryItem;
+  $new_game = new Game;
 }
 
 ?>
@@ -32,7 +46,7 @@ if(is_post_request()) {
 <main role="main" id="main-content" tabindex="-1">
     <h1>Donate A Game</h1>
 
-    <?php echo display_errors($item->errors); ?>
+    <?php echo display_errors(array_merge($item->errors, $new_game->errors)); ?>
 
     <form action="<?php echo 'donate.php'; ?>" method="post">
 
--- donate/form_fields_inventory.php
+++ donate/form_fields_inventory.php
@@ -10,19 +10,69 @@ if(!isset($item)) {
   $games = Game::getGamesAlphabetized();
   $consoles = Console::find_all();
   $users = User::find_all();
+  $ratings = AgeRating::find_all();
+  $genres = Genre::find_all();
+  $companies = Company::find_all();
 ?>
 
 <dl>
-  <dt><label for="item[id_gme_inv]">Game</label></dt>
+  <dt><label for="item[id_gme_inv]">Game (If it isn't listed, select the "Not Listed" option)</label></dt>
   <dd>
     <select name="item[id_gme_inv]" id="item[id_gme_inv]" required>
       <option value="">Select Game</option>
+      <option value="-1">Not Listed</option>
     <?php foreach($games as $game) { ?>
       <option value="<?php echo $game->id; ?>" <?php if($item->id_gme_inv == $game->id) { echo 'selected'; } ?>><?php echo $game->name_gme; ?></option>
     <?php } ?>
     </select>
   </dd>
 </dl>
+
+<fieldset id="new_game_form">
+  <legend>Can't find a game? Enter the details here.</legend>
+  <dl>
+    <dt><label for="game[name_gme]">Game Name</label></dt>
+    <dd><input type="text" name="game[name_gme]" id="game[name_gme]"></dd>
+  </dl>
+
+  <dl>
+    <dt><label for="game[id_age_gme]">ESRB Rating</label></dt>
+    <dd>
+      <select name="game[id_age_gme]" id="game[id_age_gme]">
+      <option value="">Select ESRB Rating</option>
+      <?php foreach($ratings as $rating) { ?>
+        <option value="<?php echo $rating->id; ?>"><?php echo $rating->esrb_rating_age; ?></option>
+      <?php } ?>
+      </select>
+    </dd>
+  </dl>
+
+  <dl>
+    <dt><label for="game[id_gnr_gme]">Genre</label></dt>
+    <dd>
+      <select name="game[id_gnr_gme]" id="game[id_gnr_gme]">
+      <option value="">Select Genre</option>
+      <?php foreach($genres as $genre) { ?>
+        <option value="<?php echo $genre->id; ?>"><?php echo $genre->genre_gnr; ?></option>
+      <?php } ?>
+      </select>
+    </dd>
+  </dl>
+
+  <dl>
+    <dt><label for="game[id_cmppub_gme]">Publisher</label></dt>
+    <dd>
+      <select name="game[id_cmppub_gme]" id="game[id_cmppub_gme]">
+      <option value="">Select Publisher</option>
+      <?php foreach($companies as $publisher) { ?>
+        <option value="<?php echo $publisher->id; ?>"><?php echo $publisher->name_cmp; ?></option>
+      <?php } ?>
+      </select>
+    </dd>
+  </dl>
+</fieldset>
+
+<script src="<?= url_for('js/newGame.js')?>" defer></script>
 
 <dl>
   <dt><label for="item[id_con_inv]">Console</label></dt>
