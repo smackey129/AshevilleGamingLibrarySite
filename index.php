b22e99251bb82897d4f9b739cf80730cf88b350d
spmackey <spmackey@ncsu.edu>
2023-03-26 17:18:31 -0400

    Added text to home page

--- index.php
+++ index.php
@@ -16,6 +16,9 @@ else {
 ?>
 <main role="main" id="main-content" tabindex="-1">
   <h1>Welcome to the Asheville Game Library</h1>
-  
+  <p>The Asheville Game Library is a site where you can search for and check out a variety of games donated by our users.</p>
+
+  <h2>Checking out and Returning Games</h2>
+  <p>Users can search the catalog and reserve a game if it is available. Users can check out a game for one week at a time. Once a user has reserved a game they can pick it up at our central location (this is a fictitious organization and it does not actually exist). When the week has passed users must return the game at the central location.</p>
 </main>
 <?php include(SHARED_PATH . '/user_footer.php'); ?>
