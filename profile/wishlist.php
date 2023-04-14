41942242305a92f34f1fda9b29c3d0e776ee1df1
spmackey <spmackey@ncsu.edu>
2023-04-13 20:54:20 -0400

    Added Ability to remove wishlist items

--- profile/wishlist.php
+++ profile/wishlist.php
@@ -6,8 +6,26 @@ $user = User::find_by_username($session->username);
 $wishlist = $user->getWishList();
 ?>
 
+<?php 
+  $removed = false;
+  if(is_post_request()){
+    foreach($wishlist as $item){
+      if(isset($_POST[$item->id])){
+        $user->removeFromWishList($item);
+        $removed = $item->id;
+      }
+    }
+  }
+  $wishlist = $user->getWishList();
+?>
+
 <main>
   <h1>My Wish List</h1>
+  <?php
+    if($removed) {
+      echo "<p>Item successfully removed from wishlist</p>";
+    }
+  ?>
   <div class="table">
     <table>
       <tr>
@@ -25,8 +43,14 @@ $wishlist = $user->getWishList();
           <td><?= h($item->condition_inv); ?></td>
           <td><?= h($item->getAvailability()); ?></td>
           <td><?= h($item->available_after_inv); ?></td>
+          <td>
+            <form action="wishlist.php" method="POST">
+              <input type="submit" value="Remove" name="<?= $item->id ;?>">
+            </form>
+          </td>
         </tr>
       <?php } ?>
+
     </table>
   </div>
   </div>
