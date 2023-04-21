a20a460f69f1e1d6a32ec7244768ca81e5028724
spmackey <spmackey@ncsu.edu>
2023-04-21 11:27:35 -0400

    Added Random String Generator

--- private/functions.php
+++ private/functions.php
@@ -61,4 +61,16 @@ function createSearchTerm($term, $fieldname, $database){
     return $searchString;
   }
 }
+
+function random_str(int $length = 64, string $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'): string {
+  if ($length < 1) {
+      throw new \RangeException("Length must be a positive integer");
+  }
+  $pieces = [];
+  $max = mb_strlen($keyspace, '8bit') - 1;
+  for ($i = 0; $i < $length; ++$i) {
+      $pieces []= $keyspace[random_int(0, $max)];
+  }
+  return implode('', $pieces);
+}
 ?>
