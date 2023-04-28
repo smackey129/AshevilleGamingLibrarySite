15ea080d9230eb297cb2aab987e3aded0607c8ca
spmackey <spmackey@ncsu.edu>
2023-04-28 16:44:34 -0400

    Made New Game fields required if enabled

--- js/newGame.js
+++ js/newGame.js
@@ -1,5 +1,8 @@
 const gameOption = document.querySelector("select");
 const newGameForm = document.querySelector("#new_game_form");
+const newGameSelects = newGameForm.querySelectorAll("select");
+const newGameName = newGameForm.querySelector("input");
+console.log("test");
 if (gameOption && newGameForm) {
   newGameForm.style.display = 'none';
   gameOption.addEventListener('input', toggleNewGame);
@@ -7,8 +10,16 @@ if (gameOption && newGameForm) {
 function toggleNewGame(e) {
   if(e.target.value == -1) {
     newGameForm.style.display = 'block';
+    newGameName.required = 'true';
+    newGameSelects.forEach(element => {
+      element.required = 'true';
+    });
   }
   else {
     newGameForm.style.display = 'none';
+    newGameName.removeAttribute("required");
+    newGameSelects.forEach(element => {
+      element.removeAttribute("required");
+    });
   }
 }
